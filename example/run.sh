bbd4c47416c946fce2790b0e3d4a8b6a55d7afd7
Esko Luontola <esko.luontola@solita.fi>
2013-03-19 12:38:51 +0200

    Upgraded to JRuby 1.7.3
    Closes #5

--- example/run.sh
+++ example/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Workaround for a JRuby bug: http://jira.codehaus.org/browse/JRUBY-5678
-export TMPDIR="`pwd`/tmp"
-rm -rf "$TMPDIR"
-mkdir -p "$TMPDIR"
-
 # This command will run first the built-in 'prepare' task (which processes and 
 # copies all template files) for all servers specified in example.rb, after which 
 # it will run the 'deploy' task (which is defined in example.rb) for all of them. 
